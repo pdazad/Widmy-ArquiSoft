576e4fba7273ab28fc8c540a827bbec0a0ecac77
sajimenezr1 <sa.jimenezr1@uniandes.edu.co>
2023-03-17 19:13:25 -0500

    Modelo Historias Clinicas (Incompleto)

--- widmy/historiasClinicas/models.py
+++ widmy/historiasClinicas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class HistoriaClinica(models.Model):
+    fecha_nacimiento = models.DateField()
+    fecha_ultima_actualizacion: models.DateField()
+
+
+class Reporte(models.Model):
+    fecha = models.DateField()
+    descripcion = models.CharField(max_length = 50)
+    
