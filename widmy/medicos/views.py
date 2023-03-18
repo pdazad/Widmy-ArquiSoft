e2bcfc42f23c60dd8a0ab9f675f29b2266ad521a
Nicolas-Saavedra <personal@nicolassaavedra.com>
2023-03-17 20:08:05 -0500

    Added crearPaciente

--- widmy/medicos/views.py
+++ widmy/medicos/views.py
@@ -4,22 +4,18 @@ from django.http import HttpResponseBadRequest
 
 def create_medico(request):
     if request.method == 'POST':
-        # Retrieve form data from POST request
         nombre = request.POST['nombre']
         identificacion = request.POST['identificacion']
         especialidad = request.POST['especialidad']
-        # Create new Medico instance and save to database
         medico = Medicos(
             nombre=nombre,
             identificacion=identificacion,
             especialidad=especialidad
         )
         medico.save()
-        # Redirect to list of medicos
         response_data = {'message': 'Medico created successfully.'}
         return HttpResponse(status=201, content=response_data)
     else:
-        # Render the template for creating a new medico
         return HttpResponseBadRequest('Only POST requests are allowed.')
 
 
--- widmy/pacientes/views.py
+++ widmy/pacientes/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Pacientes
+from medicos.models import Medicos
+from django.http import HttpResponse, HttpResponseBadRequest
 
-# Create your views here.
+def create_patient(request, doctor_id):
+    doctor = get_object_or_404(Medicos, id=doctor_id)
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        edad = request.POST['edad']
+        identificacion = request.POST['identificacion']
+        genero = request.POST['genero']
+        tipoSangre = request.POST['tipoSangre']
+        paciente = Pacientes(
+            historiaClinica=doctor.historiaClinica,
+            nombre=nombre,
+            edad=edad,
+            identificacion=identificacion,
+            genero=genero,
+            tipoSangre=tipoSangre
+        )
+        paciente.save()
+        response_data = {'message': 'Paciente created successfully.'}
+        return HttpResponse(status=201, content=response_data)
+    else:
+        return HttpResponseBadRequest('Only POST requests are allowed.')
