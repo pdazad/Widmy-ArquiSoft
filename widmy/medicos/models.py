907a160b811c6cb65471d9eff78def0289cb31d4
Paula Daza <p.dazad@uniandes.edu.co>
2023-03-17 19:08:01 -0500

    paciente y medico model

--- widmy/medicos/models.py
+++ widmy/medicos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Medicos(models.Model):
+    nombre = models.CharField(max_length=50)
+    identificacion = models.CharField(max_length=50)
+    especialidad = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.nombre} ({self.especialidad})'
