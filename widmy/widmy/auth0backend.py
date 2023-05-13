58312c6ddf4dc481aab805fc47457985769b6c38
sajimenezr1ST <123610011+sajimenezr1ST@users.noreply.github.com>
2023-05-12 19:49:29 -0500

    Adelantos Sprint Intermedio
    Para el uso de auth0, hay un error

--- widmy/widmy/auth0backend.py
+++ widmy/widmy/auth0backend.py
@@ -1,3 +1,4 @@
+
 import requests 
 from social_core.backends.oauth import BaseOAuth2 
 
@@ -9,30 +10,28 @@ class Auth0(BaseOAuth2):
      EXTRA_DATA = [ 
           ('picture', 'picture') 
           ] 
-     
+
      def authorization_url(self):
          """Return the authorization endpoint.""" 
          return "https://" + self.setting('DOMAIN') + "/authorize" 
-     
+
      def access_token_url(self): 
         """Return the token endpoint.""" 
         return "https://" + self.setting('DOMAIN') + "/oauth/token" 
-     
+
      def get_user_id(self, details, response): 
         """Return current user id.""" 
         return details['user_id'] 
-     
+
      def get_user_details(self, response): 
         url = 'https://' + self.setting('DOMAIN') + '/userinfo' 
         headers = {'authorization': 'Bearer ' + response['access_token']} 
         resp = requests.get(url, headers=headers) 
-        userinfo = resp.json() 
-        
-        return {'username': userinfo['nickname'],
-                'first_name': userinfo['name'],
-                'picture': userinfo['picture'], 
-                'user_id': userinfo['sub']}
-
+        userinfo = resp.json()
+        return {'username': userinfo['access_token']['nickname'],
+                'first_name': userinfo['access_token']['name'],
+                'picture': userinfo['access_token']['picture'], 
+                'user_id': userinfo['access_token']['identities'][0]['user_id']}
 def getRole(request):
     user = request.user
     auth0user = user.social_auth.get(provider="auth0")
